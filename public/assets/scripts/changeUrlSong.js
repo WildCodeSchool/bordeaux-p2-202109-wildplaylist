57a80a321c335c4aec18cb23c48f3f9e62df766b
afeynaud <alexandre.feynaud@gmail.com>
2021-11-10 15:42:00 +0100

    add url refactor service

--- public/assets/scripts/changeUrlSong.js
+++ public/assets/scripts/changeUrlSong.js
@@ -1,19 +1,8 @@
-
 const playButtons = document.getElementsByClassName('play-button');
-//const videoPlayer = document.getElementById('video-player');
-const playerContainer = document.getElementById('player-container');
+const videoPlayer = document.getElementById('video-player');
 for (const playButton of playButtons) {
-    playButton.addEventListener('click',function(){
-        const videoPlayer = document.createElement('iframe');
-        const url = playButton.dataset.url;
-        let embed = url.split("=")[1];
-        videoPlayer.src = "https://www.youtube.com/embed/" + embed;
-        videoPlayer.className="shadow-1-strong rounded";
-        videoPlayer.setAttribute('title','youtube video');
-        videoPlayer.setAttribute('allowfullscreen','true');
-        playerContainer.appendChild(videoPlayer);
+        playButton.addEventListener('click',function(){
+            console.log(playButton.dataset.url)
+            videoPlayer.src = 'https://www.youtube.com/embed/' + playButton.dataset.url;
     })
 }
-
-
-console.log(playButtons);
