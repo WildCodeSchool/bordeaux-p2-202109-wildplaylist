f9117f255b169295c3fe85c84027dc7591f01f97
afeynaud <alexandre.feynaud@gmail.com>
2021-11-15 12:42:16 +0100

    resolve urlCode error with parse_url

--- src/Service/SongUrlRefactor.php
+++ src/Service/SongUrlRefactor.php
@@ -6,21 +6,19 @@ class SongUrlRefactor
 {
     public function songUrlRefactor(string $songUrl): string
     {
+        $parameters = [''];
         if (str_starts_with($songUrl, 'https://youtu.be/') === true) {
-            $songUrl = substr($songUrl, 0, strpos($songUrl, '&ab_channel='));
             $songUrl = str_split($songUrl, 17);
             $songUrl = array_splice($songUrl, -1);
             $songUrl = implode($songUrl);
-        } elseif (str_starts_with($songUrl, 'https://www.youtube.com/watch?v=') === true) {
-            $songUrl = substr($songUrl, 0, strpos($songUrl, '&ab_channel='));
-            $songUrl = str_split($songUrl, 32);
-            $songUrl = array_splice($songUrl, -1);
-            $songUrl = implode($songUrl);
         } elseif (str_starts_with($songUrl, 'https://www.youtube.com/embed/') === true) {
-            $songUrl = substr($songUrl, 0, strpos($songUrl, '&ab_channel='));
             $songUrl = str_split($songUrl, 30);
             $songUrl = array_splice($songUrl, -1);
             $songUrl = implode($songUrl);
+        } elseif (str_starts_with($songUrl, 'https://www.youtube.com/watch?v=') === true) {
+            $queryString = parse_url($songUrl)['query'];
+            parse_str($queryString, $parameters);
+            $songUrl = $parameters['v'];
         }
         return $songUrl;
     }
