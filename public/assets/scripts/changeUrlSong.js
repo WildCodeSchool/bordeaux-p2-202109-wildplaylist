2659d9d87cb24665ee46cd44da1c983553ca60ce
afeynaud <alexandre.feynaud@gmail.com>
2021-11-21 02:35:48 +0100

    add autoplay feature

--- public/assets/scripts/changeUrlSong.js
+++ public/assets/scripts/changeUrlSong.js
@@ -3,6 +3,6 @@ const videoPlayer = document.getElementById('video-player');
 for (const playButton of playButtons) {
         playButton.addEventListener('click',function(){
             console.log(playButton.dataset.url)
-            videoPlayer.src = 'https://www.youtube.com/embed/' + playButton.dataset.url;
+            videoPlayer.src = 'https://www.youtube.com/embed/' + playButton.dataset.url + '?autoplay=1';
     })
 }
